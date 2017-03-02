7b6849716a20f5fc7dbe5e58c2a2306537d3c416
Julien Gilbert <Julienlego@gmail.com>
2017-03-01 21:42:07 -0500

    Added code to make a bar graph
    Added code to make a bar graph in plotly

--- scrape_city.py
+++ scrape_city.py
@@ -45,15 +45,63 @@ def createTable():
     index = 0
     for city in cities:
         city_state = city.capitalize() + ', ' + cities[city].capitalize()
-        table_data.append([city_state, com_rates[index], res_rates[index], ind_rates[index], avg_rates[index]])
+        city_state.replace('-', ' ')
+        table_data.append([city_state, 
+                           str(com_rates[index] + ' ¢/kWh'), 
+                           str(res_rates[index] + ' ¢/kWh'), 
+                           str(ind_rates[index] + ' ¢/kWh'), 
+                           str(avg_rates[index] + ' ¢/kWh')
+                          ])
         index += 1
     
     # Init a figure
     colorscale = [[0, '#8B5600'],[.5, '#FFEB60'],[1, '#ffffff']]
-    figure = FF.create_table(table_data, colorscale=colorscale)    #, height_constant=60)
-    figure.layout.width=720
+    figure = FF.create_table(table_data, colorscale=colorscale)
+    figure.layout.width=1000
+    #figure.layout.update({'title': 'Electricity rates in ¢/kWh'})
+    
+    # Make text size larger
+    for i in range(len(figure.layout.annotations)):
+        figure.layout.annotations[i].font.size = 16
+    
     py.iplot(figure, filename='electricity-table-rates')
 
+def createGraph():
+    city_names = []
+    for city in cities:
+        name = city.capitalize() + ', ' + cities[city].capitalize()
+        name = name.replace('-', ' ')
+        city_names.append(name)
+    
+    trace1 = go.Bar(
+                    x = city_names,
+                    y = com_rates,
+                    name = 'Com Rates'
+    )
+    trace2 = go.Bar(
+                    x = city_names,
+                    y = res_rates,
+                    name = 'Res Rates'
+    )
+    trace3 = go.Bar(
+                    x = city_names,
+                    y = ind_rates,
+                    name = 'Ind Rates'
+    )
+    trace4 = go.Bar(
+                    x = city_names,
+                    y = avg_rates,
+                    name = 'Avg Rates'
+    )
+    
+    data = [trace1, trace2, trace3, trace4]
+    layout = go.Layout(
+                       barmode = 'group'
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+    py.iplot(fig, filename = 'electricity-bar-rates')
+    
 ########################## MAIN CODE BELOW ##########################
     
 for city in cities:
@@ -61,33 +109,4 @@ for city in cities:
     scrapePage(page_url)
     time.sleep(1)
 createTable()
-
-
-# Make traces for graph
-#bar = data = [go.Bar(
-#               x = list(cities.keys()),
-#               y = cityAvgs
-#               )]
-#
-## Add trace data
-#figure['data'].extend(go.Data(bar))
-#py.iplot(data, filename='electricity-bar')
-
-#data = [ dict(
-#              type = 'scattergeo',
-#              locationmode = 'USA-states',
-#              mode = 'marker',
-#              marker = dict(
-#                            )
-#              )]
-#
-#layout = dict(
-#              title = '',
-#              colorbar = True,
-#              geo = dict(
-#                         scope = 'usa',
-#                         )
-#              )
-#    
-#fig = dict(data=data, layout=layout)
-#py.iplot(fig, filename='city-rates')
+createGraph()
