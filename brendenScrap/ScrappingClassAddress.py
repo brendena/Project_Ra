b44e31db08f8e4b41880680dbf86d19da577edee
brendena <cerebralasylum1@gmail.com>
2017-03-05 13:54:24 -0500

    split up csv files and working on long run of data scrapping

--- brendenScrap/ScrappingClassAddress.py
+++ brendenScrap/ScrappingClassAddress.py
@@ -5,14 +5,20 @@
 |   http://pythonhosted.org/uszipcode/#by-coordinate
 /////////////////////////////////////////////////////////////////'''
 from pygeocoder import Geocoder
-from random import uniform 
+from pygeocoder import GeocoderError
+from random import uniform
+import random 
 from Render import JSRender
 import pandas as pd
 from uszipcode import ZipcodeSearchEngine
+import time
+
 import pickle
 import json
 import csv
 import os.path
+import sys
+from pathlib import Path
 
 
 class ScrappingClassAddress:
@@ -24,6 +30,12 @@ class ScrappingClassAddress:
         self.listFerida = json.loads(open(self.dataFolder + "/listFerida.json" ).read())['listFeridaFiles']
         self.ListPlacesCanScrape = json.loads(open("listPlacesScrape.json").read())
         self.search = ZipcodeSearchEngine()
+        self.GoogleTimeLog = 0;
+        self.keys = ["AIzaSyACVu-9cEH1KkHdnImX7SyZl--7wipuRP4",
+                     "AIzaSyDGVCEUtFSRp7WZ6UgmzWfBeOAF3iiw_hc",
+                     "AIzaSyAvQMM_9jGdFq7u2nBcNu5jxIXE4HUfa_A",
+                     "AIzaSyCkea01k1qu4r9rGVzbkh29MMKGSt6mBgE",
+                     "AIzaSyDt8KzzEJk4UDrBXS8kHwhQREhfoWFNdWA"]
         
   
     def searchURL(self, address):
@@ -32,7 +44,7 @@ class ScrappingClassAddress:
         #url = "https://www.google.com/get/sunroof#a=15%20Glendale%20Ave,%20Somerville,%20MA%2002144,%20USA&b=150&f=lease&np=28&p=1&sh=1"
 
         url = "https://www.google.com/get/sunroof#a="
-        print(url + str(address) + "&p=1")
+        #print(url + str(address) + "&p=1")
         return url + str(address) + "&p=1" 
         
                 
@@ -47,17 +59,37 @@ class ScrappingClassAddress:
     def loopThroughAllAddress(self):
 
         for fileName in self.listFerida:
-            print("./" + self.dataFolder + "/" + fileName)
-            df = pd.read_csv("./" + self.dataFolder + "/" + fileName + ".csv")
-            output = csv.writer(open("./" + self.dataFolder + "/" + fileName + "Updated.csv",'w'))
+            print("./" + self.dataFolder + "/" + fileName)                      #speeds up things a lot and uses less memory
+            baseFile = "./" + self.dataFolder + "/" + fileName
+            df = pd.read_csv(baseFile + ".csv", usecols=['id','latitude','longitude'])
+            
+            #if file exists - add
+            if(Path(baseFile + "Updated.csv").is_file()):
+                csvFile = open(baseFile+ "Updated.csv",'w')
+                output = csv.writer(csvFile)
+                print(sum(1 for row in csv.reader(baseFile+ "Updated.csv")))
+
+            #if file doesn't - start fresh
+            else:
+                csvFile = open(baseFile+ "Updated.csv",'w')
+                output = csv.writer(csvFile)
+                output.writerow(["id","hoursSun","sqFtRoof"])
+                startingPoint = 0
+
+
+
+            
             zipCodes = []
             for i in range(0,len(self.ListPlacesCanScrape[fileName])):
                 zipCodes.append(int(self.ListPlacesCanScrape[fileName][i]["zipCode"]))
 
             print("totalNumber ",df["id"].count())
 
-            for i in range(0,5): #range(df["id"].count()): 
-                self.loopLogic(output,df.iloc[[i]],zipCodes)
+            count = df["id"].count()
+            
+            for i in range(count): #range(0,200): 
+                print(i," / ", count)
+                self.loopLogic(output,df.iloc[i],zipCodes)
                                     #there something wrong with the iloc
                                     #it doesn't look right when you take the lat and longtidue out
                 
@@ -67,39 +99,53 @@ class ScrappingClassAddress:
         lat = datapoint['latitude']
         lng = datapoint['longitude']
         ID = datapoint['id']
-        print(lat)
-        print(lng)
+
         
-        #offlineCheck = self.offlineZipCodeCheck(lat,lng,zipCodes)
-        '''
+        offlineCheck = self.offlineZipCodeCheck(lat,lng,zipCodes)
+        
         if(offlineCheck != 0 ):
             address = self.googleGetZipCode(lat,lng,zipCodes)
             if(address != 0):
                 self.getZipCodeData(output,address,ID)
+            else:
+                output.writerow([ID, 0, 0])
         else:
-            output.writerow()
-        '''
+            output.writerow([ID, 0, 0])
+        
+        
 
     def getZipCodeData(self,output,address,ID):
         url = self.searchURL(address)
         data = self.JSRender.getAddressPageInfo(url)
         data["id"] = ID
-        #if(count == 0 ):
-        #    header = data.keys()
-        #    output.writerow(header)
-        #    count = 1
+        #print(data.values())
+        output.writerow([data["id"], data["hoursSun"], data["sqFtRoof"]])
         
-        output.writerow(data.values())
+        
     def googleGetZipCode(self,latitude, longitude,zipCodes):
-        address = Geocoder.reverse_geocode(latitude, longitude)
-        print(address.postal_code)
-        if(int(address.postal_code) in zipCodes):
-            return 0
+        count = 0
+        if(self.GoogleTimeLog - time.time() < .5):
+            time.sleep(1)
+        self.GoogleTimeLog = time.time()
+        while(True):
+            try:
+                address = Geocoder(random.choice(self.keys)).reverse_geocode(latitude, longitude)
+                break 
+            except GeocoderError as GeoError:
+                print(GeoError)
+                count = count + 1
+                time.sleep(10)
+                if(count > 3):
+                    sys.exit()
+
+        if(address.postal_code is not None and 
+            int(address.postal_code) in zipCodes):
+
+            return address 
         else:
-            return address
+            return 0
     def offlineZipCodeCheck(self,latitude, longitude,zipCodes):
         zipCode = self.search.by_coordinate(latitude, longitude, radius=2,returns=2)
-        '''
         if(len(zipCode) == 0):
             return 2
         else:
@@ -108,4 +154,4 @@ class ScrappingClassAddress:
                 return 1
             else:
                 return 0
-        '''
+        
--- brendenScrap/main.py
+++ brendenScrap/main.py
@@ -1,12 +1,13 @@
 from ScrappingClassAddress import ScrappingClassAddress 
 from ScrappingClassZipCodes import ScrappingClassZipCode
 
-
+import time
 SC = ScrappingClassAddress()
 SC.loopThroughAllAddress()
 
 #ZC = ScrappingClassZipCode()
 #ZC.loopThroughAllAddress()
+
 
 
 
